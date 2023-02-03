d8a12a5424f19d4af93d456ce46e45c1b0637f4e
JuanMa <mitild82@gmail.com>
2023-02-03 10:10:03 +0100

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,7 +2,7 @@ export default function Nav() {
     return (
         <nav className="nav-bar">
             <img className="nav-logo" src="/logo.png" />
-            <p className="nav-text">my traveling journal.</p>
+            <p className="nav-text">my travel journal.</p>
         </nav>
     )
 }
