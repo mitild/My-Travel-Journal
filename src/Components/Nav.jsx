fce0701611396b3778f9d5124c6115dc1fe50460
JuanMa <mitild82@gmail.com>
2023-02-03 10:04:33 +0100

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav() {
     return (
         <nav className="nav-bar">
-            <img className="nav-logo" src="/src/assets/logo.png" />
+            <img className="nav-logo" src="../src/assets/logo.png" />
             <p className="nav-text">my travel journal.</p>
         </nav>
     )
